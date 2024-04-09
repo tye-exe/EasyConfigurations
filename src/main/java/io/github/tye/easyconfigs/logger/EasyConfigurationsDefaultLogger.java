7a2011d656b2ede260aa875fe7fae3e6e57f6bb7
tye <tye@mailbox.org>
2024-04-09 19:28:45 +0100

    Added javadocs to logger.
    Documented how to override the default logger inside the appropriate classes.

--- src/main/java/io/github/tye/easyconfigs/logger/EasyConfigurationsDefaultLogger.java
+++ src/main/java/io/github/tye/easyconfigs/logger/EasyConfigurationsDefaultLogger.java
@@ -10,11 +10,18 @@ import java.util.logging.Logger;
  This class is the default logging class for EasyConfigurations. It performs logging based upon the
  default java {@link Logger logger}.
  <p>
- If you want to implement a custom logger (this is usually to redirect the text output)... TODO */
+ If you want to implement a custom logger (this is usually to redirect the text output)... TODO
+ <p>
+ The default logger is a default java logger created by {@link Logger#getLogger(String)} with a name
+ of {@code io.github.tye.easyconfigs.EasyConfigurations} */
 @ExternalUse
 public class EasyConfigurationsDefaultLogger implements EasyConfigurationsLogger {
 
-
+/**
+ Contains the default logger used by EasyConfigurations. To override this logger use
+ {@link
+ io.github.tye.easyconfigs.EasyConfigurations#overrideEasyConfigurationsLogger(EasyConfigurationsLogger)
+ EasyConfigurations#overrideEasyConfigurationsLogger(EasyConfigurationsLogger)} */
 @InternalUse
 public static @NotNull EasyConfigurationsLogger logger = new EasyConfigurationsDefaultLogger();
 
@@ -25,9 +32,13 @@ public static @NotNull EasyConfigurationsLogger logger = new EasyConfigurationsD
 private static final @NotNull Logger javaLogger = Logger.getLogger("io.github.tye.easyconfigs.EasyConfigurations");
 
 
+/**
+ Logs the message to the default java logger.
+ @param logType    The type of log message to output.
+ @param logMessage The text that will be included in the log message. */
 @Override
 @InternalUse
-public void log(LogType logType, String logMessage) {
+public void log(@NotNull LogType logType, @NotNull String logMessage) {
   javaLogger.log(logType.getLogLevel(), logMessage);
 }
 
--- src/main/java/io/github/tye/easyconfigs/logger/EasyConfigurationsLogger.java
+++ src/main/java/io/github/tye/easyconfigs/logger/EasyConfigurationsLogger.java
@@ -1,7 +1,25 @@
 package io.github.tye.easyconfigs.logger;
 
+import io.github.tye.easyconfigs.annotations.ExternalUse;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ This interface contains the methods necessary to override the default logger for
+ EasyConfigurations.
+ <p>
+ To override the logger, implement this method in a class, then call {@link
+io.github.tye.easyconfigs.EasyConfigurations#overrideEasyConfigurationsLogger(EasyConfigurationsLogger)
+overrideEasyConfigurationsLogger} in
+ {@link io.github.tye.easyconfigs.EasyConfigurations EasyConfigurations}.
+ <p>
+ To see the default logging behaviour view {@link EasyConfigurationsDefaultLogger}. */
+@ExternalUse
 public interface EasyConfigurationsLogger {
 
-void log(LogType logType, String logMessage);
+/**
+ Logs a message to the EasyConfigurations logger.
+ @param logType    The type of log message to output.
+ @param logMessage The text that will be included in the log message. */
+void log(@NotNull LogType logType, @NotNull String logMessage);
 
 }
